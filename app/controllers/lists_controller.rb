30b9cc179138200c3e84993a0d0f90970548aa0b
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-03-18 15:02:38 +0000

    rails_9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,23 @@
 class ListsController < ApplicationController
-  def new
-    @list = List.new
-  end
-  
-def create 
-  list = List.new(list_params)
-  list.save
-  redirect_to list_path(list.id)
+
+def new
+@list = List.new
 end
-  
-  
-  
+
+
+def create
+ @list = List.new(list_params)
+  if @list.save
+  redirect_to list_path(@list.id)
+else
+  render :new
+end
+
+end
+
+
+
+
   def index
     @lists = List.all
   end
@@ -20,21 +27,29 @@ end
   end
 
   def edit
-  
-  @list = List.find(params[:id])  
-    
+
+  @list = List.find(params[:id])
+
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
-  
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+
+
+  end
+
+
 private
 def list_params
-params.require(:list).permit(:title, :body)
+params.require(:list).permit(:title, :body, :image)
 end
 
 
